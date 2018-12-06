14308b794cfe7682f2f86e885504db529d5c97c0
thehoten <michaelhutton01@hotmail.com>
2018-12-06 16:14:11 +0000

    Add POST /todos route and refractor mongoose

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser=require('body-parser');
+var {Todo}=require('../models/Todo');
+
+router.use(bodyParser.json());
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  mongoose.connect('mongodb://localhost:27017/tuts',{ useNewUrlParser: true });
+  // var User=require('../models/users');
+  // var user=new User({
+  //   email:'michaelhutton01@hotmail.com'
+  // });
+  // console.log(user);
+  // user.save().then((doc)=>{
+  //   console.log('User saved',doc);
+  // },(e)=>{
+  //   console.log('Unable to save user',e);
+  // });
+  res.render('index', { title: 'Express' });
+
+});
+
+router.post('/todos',(req,res)=>{
+  var todo=new Todo({text:req.body.text});
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+  console.log(req.body);
+});
+
+function exit(){
+  mongoose.disconnect();
+}
+
+module.exports = router;
